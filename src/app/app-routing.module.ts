f981b48251b729fd6291b77c7c36c61a3ec2d913
anupam897 <anupam.kushwaha@idreambiz.com>
2021-04-19 17:04:15 +0530

    second commit

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,17 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { CartComponent } from './cart/cart.component';
+import { LoginComponent } from './login/login.component';
+import { ProductsComponent } from './products/products.component';
+
+const routes: Routes = [
+  {path:'login', component:LoginComponent},
+  {path:'product', component:ProductsComponent},
+  {path:'cart', component:CartComponent}
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
